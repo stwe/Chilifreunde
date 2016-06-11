7ccb97749f0e9f56599f8aec4ae6439c4e6bd565
Steffen <ursus.maritimus2.0@gmail.com>
2016-06-11 14:56:35 +0200

    compiler pass added

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -2,8 +2,22 @@
 
 namespace AppBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use AppBundle\DependencyInjection\Compiler\FOSUserOverridePass;
 
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class AppBundle
+ *
+ * @package AppBundle
+ */
 class AppBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new FOSUserOverridePass());
+    }
 }
